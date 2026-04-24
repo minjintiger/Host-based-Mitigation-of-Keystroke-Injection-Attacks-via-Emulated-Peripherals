3d1ba0b4dff08b62d202cc4187fd99d307c074cf
darmorack <darmorack@gmail.com>
2026-04-24 16:02:51 -0400

    Started creating UI to view and adjust DB

---
+++ daemon/user_settings.py
@@ -0,0 +1,42 @@
+"""
+user_settings.py
+
+Allows user to interact with database such as viewing and adjusting blacklist or logs.
+"""
+
+
+import sqlite3
+from queries import QueryService as QS
+import streamlit as st
+from enum import StrEnum
+
+class Table(StrEnum):
+    STRINGS = "String Blacklist"
+    HOTKEY = "Hotkey Blacklist"
+    PAIR = "Hotkey String Pair Blacklist"
+    BLACKLIST_DETECTION = "Blacklist Detection Log"
+    KEYSTROKE_DETECTION = "Keystroke Detection Log"
+
+table = st.selectbox("Select a table to view:", Table)
+
+match table:
+    case Table.STRINGS:
+        st.write("You selected String Blacklist")
+    case Table.HOTKEY:
+        st.write("hotkeys")
+    case Table.PAIR:
+        st.write("pairs")
+    case Table.BLACKLIST_DETECTION:
+        st.write("blacklist log")
+    case Table.KEYSTROKE_DETECTION:
+        st.write("keystroke log")
+    case _:
+        st.write("Not implemented!")
+
+
+
+
+
+
+
+
